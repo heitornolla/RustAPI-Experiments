b1719e57165c35e99942c05bdd61c179da63aab1
heitor <heitor.nolla@gmail.com>
2025-06-30 15:55:11 -0300

    began async session

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,10 @@ from contextlib import contextmanager
 from datetime import datetime
 
 import pytest
+import pytest_asyncio
 from fastapi.testclient import TestClient
-from sqlalchemy import StaticPool, create_engine, event
-from sqlalchemy.orm import Session
+from sqlalchemy import StaticPool, event
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from fastapi_project.app import app
 from fastapi_project.database import get_session
@@ -25,14 +26,17 @@ def client(session):
     app.dependency_overrides.clear()
 
 
-@pytest.fixture
-def session():
-    engine = create_engine(
-        'sqlite:///:memory:', connect_args={'check_same_thread': False}, poolclass=StaticPool
+@pytest_asyncio.fixture
+async def session():
+    engine = create_async_engine(
+        'sqlite+aiosqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
     )
 
     table_registry.metadata.create_all(engine)
-    with Session(engine) as session:
+
+    async with AsyncSession(engine) as session:
         yield session
 
     table_registry.metadata.drop_all(engine)
