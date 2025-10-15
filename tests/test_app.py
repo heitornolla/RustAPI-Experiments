02d44a37b41974a24cc1b182644ca56c23390780
heitor <heitor.nolla@gmail.com>
2025-10-15 12:37:05 -0300

    refactoring for rustapi

--- tests/test_app.py
+++
@@ -1,8 +0,0 @@
-from http import HTTPStatus
-
-
-def test_root_hello_world(client):
-    response = client.get('/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
